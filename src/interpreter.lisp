0160fc9c7c3029db7a55bc1cba4779750bb3ba1b
Masataro Asai <guicho2.71828@gmail.com>
2017-11-09 05:04:19 +0900

    slight refactoring

--- src/interpreter.lisp
+++ src/interpreter.lisp
@@ -39,21 +39,23 @@
   "Flag for debugging the input to the prolog interpreter.
  Enables verbose output when something is sent the interpreter.")
 
+(defun %print-rule (stream rule)
+  (when *debug-prolog*
+    (format t "~&; ~/cl-prolog::print-rule/" rule))
+  (print-rule stream rule nil nil))
+
 (defun send-rule (process rule)
   (send-rules process (list rule)))
 
-#+(or)                                  ;doesnt seem to recognize EOT characters.
+#+(or)
+;; doesnt seem to recognize EOT characters.
 (defun send-rules (process rules)
   (with-prolog-io (process i o e)
     ;; enter the interactive mode
-    (when *debug-prolog*
-      (format t "~&; ~/cl-prolog::print-rule/" '(list user)))
-    (print-rule i '(list user) nil nil)
+    (%print-rule i '(list user))
     ;; enter rules
     (dolist (r rules)
-      (when *debug-prolog*
-        (format t "~&; ~/cl-prolog::print-rule/" r))
-      (print-rule i r nil nil))
+      (%print-rule i r))
     (when *debug-prolog*
       (format t "~&; EOT"))
     (write-char #\EOT i)
@@ -69,18 +71,14 @@
     ;;   (format t "~&; ~/cl-prolog::print-rule/" `(assertz (and ,@rules))))
     ;; (print-rule i  `(assertz (and ,@rules)) nil nil)
     (dolist (r rules)
-      (when *debug-prolog*
-        (format t "~&; ~/cl-prolog::print-rule/" `(assertz ,r)))
-      (print-rule i  `(assertz ,r) nil nil)
+      (%print-rule i `(assertz ,r))
       (finish-output i)
       (clear-input o))))
 
 
 (defun send-query (process query callback)
   (with-prolog-io (process i o e)
-    (when *debug-prolog* 
-      (format t "~&; ~/cl-prolog::print-rule/" query))
-    (print-rule i query nil nil)
+    (%print-rule i query)
     (finish-output i)
     (unwind-protect
          (loop
