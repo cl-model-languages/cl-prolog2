9e135c3c5995e6eba2b034d15d3102fd7f426094
Masataro Asai <guicho2.71828@gmail.com>
2017-12-22 11:13:55 +0900

    changed the default extension for the prolog output

--- bprolog/package.lisp
+++ bprolog/package.lisp
@@ -14,7 +14,7 @@
 
 (defmethod run-prolog ((rules list) (prolog-designator (eql :bprolog)) &key debug args)
   (with-temp (d :directory t :debug debug)
-    (with-temp (input-file :tmpdir d :template "XXXXXX.pl" :debug debug)
+    (with-temp (input-file :tmpdir d :template "XXXXXX.prolog" :debug debug)
       (with-open-file (s input-file :direction :output :if-does-not-exist :error)
         (let ((*debug-prolog* debug))
           (dolist (r rules)
